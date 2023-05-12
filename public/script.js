2a6ac983ca082dc6c5e743c951d00f4c94818db5
Cooper <coopcoopmoy06@gmail.com>
2023-05-12 16:06:14 -0600

    semantic search

--- public/script.js
+++ public/script.js
@@ -1,10 +1,10 @@
 let messageHistory = [
     {
-        "role": "system",
-        "content": "You are a helpful assistant that provides detailed and accurate information."
-    }
+        role: "system",
+        content:
+            "You are a helpful assistant that provides detailed and accurate information.",
+    },
 ];
-
 
 async function searchProductsByTags(theme, style, color = "") {
     let combinedTags = `tag:'${theme}' AND tag:'${style}'`;
@@ -12,41 +12,42 @@ async function searchProductsByTags(theme, style, color = "") {
         combinedTags += ` AND tag:'${color}'`;
     }
     const query = `
-{
-products(first: 5, query: "${combinedTags} AND status:active") {
-    edges {
-        node {
-            id
-            title
-            handle
-            featuredImage {
-                transformedSrc(maxWidth: 100, maxHeight: 100, crop: CENTER)
-            }
-        }
-    }
-}
-}`;
-
+  {
+  products(first: 5, query: "${combinedTags} AND status:active") {
+      edges {
+          node {
+              id
+              title
+              handle
+              featuredImage {
+                  transformedSrc(maxWidth: 100, maxHeight: 100, crop: CENTER)
+              }
+          }
+      }
+  }
+  }`;
 
     try {
-        const response = await fetch('/shopify-api', {
-            method: 'POST',
+        const response = await fetch("/shopify-api", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({ query: query })
+            body: JSON.stringify({ query: query }),
         });
 
         const jsonResponse = await response.json();
         if (jsonResponse.data && jsonResponse.data.products) {
-            return jsonResponse.data.products.edges.slice(0, 5).map(edge => {
-                return {
-                    id: edge.node.id,
-                    title: edge.node.title,
-                    handle: edge.node.handle,
-                    imageUrl: edge.node.featuredImage.transformedSrc
-                };
-            });
+            return jsonResponse.data.products.edges
+                .slice(0, 5)
+                .map((edge) => {
+                    return {
+                        id: edge.node.id,
+                        title: edge.node.title,
+                        handle: edge.node.handle,
+                        imageUrl: edge.node.featuredImage.transformedSrc,
+                    };
+                });
         }
     } catch (error) {
         console.error("Error fetching products:", error);
@@ -54,21 +55,17 @@ products(first: 5, query: "${combinedTags} AND status:active") {
     return [];
 }
 
-
-
-
-
 const userAction = async (event) => {
     event.preventDefault();
     const input = "" + document.forms["input"]["input"].value;
 
-    messageHistory.push({ "role": "user", "content": input });
+    messageHistory.push({ role: "user", content: input });
 
     // Disable input field and Send button while processing request
     document.forms["input"]["input"].disabled = true;
     document.forms["input"]["submit"].disabled = true;
 
-    const response = await fetch('/api/completions', {
+    const response = await fetch("/api/completions", {
         method: "POST",
         body: JSON.stringify({ messageHistory: messageHistory }),
         headers: {
@@ -93,42 +90,51 @@ const userAction = async (event) => {
 
     if (tags.length > 0) {
         const [theme, style, color] = tags;
-        searchProductsByTags(theme, style, color).then(product_links => {
-            const displayMessage = message;
+        searchProductsByTags(theme, style, color)
+            .then((product_links) => {
+                const displayMessage = message;
 
-            const AnswerLog = messageHistory.map((msg, index) => {
-                if (msg.role === "system") return "";
-                return `<br><br>${msg.role === 'user' ? 'ME' : 'AI'}: ${msg.content}`;
+                const AnswerLog = messageHistory.map((msg, index) => {
+                    if (msg.role === "system") return "";
+                    return `<br><br>${msg.role === "user" ? "ME" : "AI"
+                        }: ${msg.content}`;
+                });
+
+                if (product_links.length > 0) {
+                    const product_links_html = product_links
+                        .slice(0, 5)
+                        .map(
+                            (link) => `
+      <a href="https://taylorjoelle.com/products/${link.handle}" target="_blank" style="text-decoration: none; color: inherit;">
+          <div class="product-container">
+              <img src="${link.imageUrl}" alt="${link.title}">
+              <div class="product-title">${link.title}</div>
+          </div>
+      </a>
+      `
+                        )
+                        .join("");
+                    message += "<br><br>" + product_links_html;
+                }
+                messageHistory.push({ role: "assistant", content: displayMessage });
+
+                document.forms["input"]["input"].value = "";
+                document.getElementById("output").innerHTML =
+                    AnswerLog.join("") + `<br><br>AI: ${message}`;
+
+                // Re-enable input field and Send button after processing request
+                document.forms["input"]["input"].disabled = false;
+                document.forms["input"]["submit"].disabled = false;
+            })
+            .catch((error) => {
+                console.error("Error displaying product links:", error);
             });
-
-            if (product_links.length > 0) {
-                const product_links_html = product_links.slice(0, 5).map(link => `
-    <a href="https://taylorjoelle.com/products/${link.handle}" target="_blank" style="text-decoration: none; color: inherit;">
-        <div class="product-container">
-            <img src="${link.imageUrl}" alt="${link.title}">
-            <div class="product-title">${link.title}</div>
-        </div>
-    </a>
-    `).join("");
-                message += "<br><br>" + product_links_html;
-            }
-            messageHistory.push({ "role": "assistant", "content": displayMessage });
-
-            document.forms["input"]["input"].value = "";
-            document.getElementById("output").innerHTML = AnswerLog.join("") + `<br><br>AI: ${message}`;
-
-            // Re-enable input field and Send button after processing request
-            document.forms["input"]["input"].disabled = false;
-            document.forms["input"]["submit"].disabled = false;
-        }).catch((error) => {
-            console.error("Error displaying product links:", error);
-        });
     } else {
-        messageHistory.push({ "role": "assistant", "content": aiReply });
+        messageHistory.push({ role: "assistant", content: aiReply });
 
         const AnswerLog = messageHistory.map((msg, index) => {
             if (msg.role === "system") return "";
-            return `<br><br>${msg.role === 'user' ? 'ME' : 'AI'}: ${msg.content}`;
+            return `<br><br>${msg.role === "user" ? "ME" : "AI"}: ${msg.content}`;
         });
 
         // Re-enable input field and Send button after processing request
@@ -137,19 +143,16 @@ const userAction = async (event) => {
     }
 };
 
-
-
-
 const clearHistory = () => {
     const newSystemMessage = document.forms["systemMessageForm"]["systemMessage"].value;
     messageHistory = [
         {
-            "role": "system",
-            "content": newSystemMessage
-        }
+            role: "system",
+            content: newSystemMessage,
+        },
     ];
     document.getElementById("output").innerHTML = "";
-}
+};
 
 const addCustomOption = () => {
     const customOption = document.forms["addOptionForm"]["customOption"].value;
@@ -162,4 +165,4 @@ const addCustomOption = () => {
     selectElement.add(newOption);
 
     document.forms["addOptionForm"]["customOption"].value = ""; // Clear the input field
-}
+};
--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import fetch from 'node-fetch';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import dotenv from "dotenv";
+import natural from "natural";
 
 dotenv.config();
+
+const TfIdf = natural.TfIdf;
+const tfidf = new TfIdf();
 
 const PORT = process.env.PORT;
 const mongodbUri = process.env.MONGODB_URI;
@@ -18,139 +22,135 @@ const PASSWORD = process.env.PASSWORD;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, "public")));
 
-import { readFile } from 'fs/promises';
+let allProducts = [];
 
-app.get('/independent.html', async (req, res) => {
-  try {
-    const fileContent = await readFile(join(__dirname, 'public', 'independent.html'), 'utf8');
-    res.send(fileContent);
-  } catch (error) {
-    res.status(500).send('Error: Cannot find the independent.html file');
-  }
-});
-
-app.get('/shopify-products', async (req, res) => {
-  try {
-    const products = await getAllProducts();
-    const categorizedProducts = categorizeProducts(products);
-    console.log('sorted',categorizeProducts);
-    res.json(categorizedProducts);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-async function fetchProducts() {
-  try {
-    const response = await fetch('/shopify-products', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
+const fetchProducts = async () => {
+  const query = `
+    {
+      products(first: 250, query: "status:active") {
+        edges {
+          node {
+            id
+            title
+            handle
+            descriptionHtml
+            tags
+          }
+        }
       }
+    }
+  `;
+
+  const url = `https://${SHOP_NAME}.myshopify.com/admin/api/2023-01/graphql.json`;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "X-Shopify-Access-Token": ACCESS_TOKEN,
+    Authorization:
+      "Basic " +
+      Buffer.from(API_KEY + ":" + PASSWORD).toString("base64"),
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({ query: query }),
     });
 
     const jsonResponse = await response.json();
-    console.log(jsonResponse);
-    
-    const categorizedProducts = {};
 
-    jsonResponse.forEach(product => {
-      const relevantTags = product.tags.filter(tag => {
-        return !/^cf-size-/.test(tag) && !/^all/.test(tag) && !/^3053/.test(tag) && !/^dress/.test(tag) && !/^classic/.test(tag) && !/^childrens/.test(tag);
-      }).slice(0, 5);
-
-      relevantTags.forEach(tag => {
-        if (!categorizedProducts[tag]) {
-          categorizedProducts[tag] = [];
-        }
-
-        // Check if the product is already in the category
-        const productExists = categorizedProducts[tag].some(existingProduct => existingProduct.title === product.title);
-
-        if (!productExists) {
-          categorizedProducts[tag].push({
-            title: product.title,
-            tags: relevantTags,
-          });
+    if (
+      jsonResponse &&
+      jsonResponse.data &&
+      jsonResponse.data.products &&
+      jsonResponse.data.products.edges
+    ) {
+      jsonResponse.data.products.edges.forEach((productEdge, i) => {
+        if (productEdge && productEdge.node) {
+          const product = productEdge.node;
+          allProducts.push(product);
+          tfidf.addDocument(
+            `${product.title} ${product.tags.join(" ")}`
+          );
+        } else {
+          console.error("Unexpected productEdge:", productEdge);
         }
       });
-    });
-
-    return categorizedProducts;
+    } else {
+      console.error("Unexpected jsonResponse:", jsonResponse);
+    }
   } catch (error) {
-    console.error("Error fetching products:", error);
-    return [];
+    console.error("Error fetching products:", error.message);
   }
-}
+};
 
+fetchProducts();
 
-function categorizeProducts(products) {
-  const categories = {};
+import { readFile } from "fs/promises";
 
-  products.forEach(product => {
-    product.tags.forEach(tag => {
-      tag = tag.toLowerCase();
+app.get("/independent.html", async (req, res) => {
+  try {
+    const fileContent = await readFile(
+      join(__dirname, "public", "independent.html"),
+      "utf8"
+    );
+    res.send(fileContent);
+  } catch (error) {
+    res.status(500).send("Error: Cannot find the independent.html file");
+  }
+});
 
-      if (!categories[tag]) {
-        categories[tag] = [];
-      }
-
-      categories[tag].push(product);
-    });
-  });
-
-  return categories;
-}
-
-
-
-app.post('/api/completions', async (req, res) => {
+app.post("/api/completions", async (req, res) => {
   const messageHistory = req.body.messageHistory;
   const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
   try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: 'POST',
-      body: JSON.stringify({ 'model': 'gpt-3.5-turbo', 'messages': messageHistory }),
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: messageHistory,
+      }),
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
       },
     });
 
     const jsonResponse = await response.json();
     res.send(jsonResponse);
-
   } catch (error) {
-    console.error('Error with API call:', error);
-    res.status(500).send('Error with API call');
+    console.error("Error with API call:", error);
+    res.status(500).send("Error with API call");
   }
 });
 
-app.post('/shopify-api', async (req, res) => {
+app.post("/shopify-api", async (req, res) => {
   const query = req.body.query;
 
   const url = `https://${SHOP_NAME}.myshopify.com/admin/api/2023-01/graphql.json`;
 
   const headers = {
-    'Content-Type': 'application/json',
-    'X-Shopify-Access-Token': ACCESS_TOKEN,
-    'Authorization': 'Basic ' + Buffer.from(API_KEY + ':' + PASSWORD).toString('base64')
+    "Content-Type": "application/json",
+    "X-Shopify-Access-Token": ACCESS_TOKEN,
+    Authorization:
+      "Basic " +
+      Buffer.from(API_KEY + ":" + PASSWORD).toString("base64"),
   };
 
   try {
     console.log(query);
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: headers,
-      body: JSON.stringify({ query: query })
+      body: JSON.stringify({ query: query }),
     });
 
     const jsonResponse = await response.json();
@@ -159,6 +159,28 @@ app.post('/shopify-api', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+app.get("/products", (req, res) => {
+  if (req.query.query) {
+    const searchTerm = req.query.query;
+    const results = [];
+
+    tfidf.tfidfs(searchTerm, function (i, measure) {
+      results.push({ index: i, measure });
+    });
+
+    results.sort((a, b) => b.measure - a.measure);
+    const topResults = results.slice(0, 5).map((result) => allProducts[result.index]);
+
+    res.render("search", { products: topResults, query: req.query.query });
+  } else {
+    res.render("search", { products: allProducts });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("search");
 });
 
 app.listen(PORT, () => {
